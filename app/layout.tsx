1277dfd5d9d8c39b99a48bcc720bf8f8b45c3631
Minyoung Lee <zhal7779@gmail.com>
2023-11-06 22:13:18 +0900

    Rename: 디렉토리 구조 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,4 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '26cm | 감도 깊은 취향 셀렉트 샵',
@@ -16,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '26cm | 감도 깊은 취향 셀렉트 샵',
+  description: '감도 깊은 취향 셀렉트 샵 26cm',
+};
+
+export default function Home() {
+  return <p>26CM</p>;
+}
